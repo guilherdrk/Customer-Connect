8460437c980ffd961e48c518b07d12c39cf9579f
guilherdrk <guibem20@gmail,com>
2025-11-05 11:53:58 -0300

    first commit

---
+++ src/test/java/com/guilherdrk/customerconnect/CustomerconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherdrk.customerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
