d7d173c8a4defe94a86fe9abf7967bb5b848dbef
guilherdrk <guibem20@gmail,com>
2025-11-05 13:37:50 -0300

    Criando a @Entity Customer

---
+++ src/main/java/com/guilherdrk/customerconnect/entity/CustomerEntity.java
@@ -0,0 +1,75 @@
+package com.guilherdrk.customerconnect.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer")
+public class CustomerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "cell_phone")
+    private String cellPhone;
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+
+    public CustomerEntity() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+}
